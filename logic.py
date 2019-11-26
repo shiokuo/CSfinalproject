4fa28eb490f89e756d461bdd1efc2053c415f186
unknown <a0966530827@gmail.com>
2019-11-26 17:38:41 +0800

    main

--- logic.py
+++ logic.py
@@ -1,16 +1,15 @@
-import main.factor as fac
+import main
 def hit(status):
 	if status == 1:
-		fac[life_ob[0]]-=fac[atk]*fac[atk_up]
+		main.factor[life_ob[0]]-=main.factor[atk]*main.factor[atk_up]
 		print('Beat the monster: "ahah ahhhh..." The monster loses %f points of life.'% (atk*atk_up))
-		fac[atk_up]=1	
-	return fac,1
+		main.factor[atk_up]=1	
+	return 1
 def charge(status):
-	fac[atk_up]*=1.2
-	return fac,2
+	main.factor[atk_up]*=1.2
+	return 2
 	print("Next round, you'll attack the monster 0.2 stronger than you will originally.")
 def react(skill_use,status):
 	if status == 1:
-		fac[atk_ob[0]] -= fac[atk_ob]*fac[atk_ob_up]
+		main.factor[atk_ob[0]] -= main.factor[atk_ob]*main.factor[atk_ob_up]
 		print('The monster is attacking you... Be careful...\n"Ahhh"')
-	return fac
