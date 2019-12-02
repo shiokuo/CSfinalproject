d6be59f54084c95098a052a20fb730a9d7424728
unknown <a0966530827@gmail.com>
2019-12-02 16:30:22 +0800

    fossjfpdf

--- logic.py
+++ logic.py
@@ -1,15 +1,13 @@
-def hit(status):
-	if status == 1:
-		factor[life_ob[0]] -= factor[atk]*factor[atk_up]
-		print('Beat the monster: "ahah ahhhh..." The monster loses %f points of life.'% (atk*atk_up))
-		factor[atk_up]=1	
+def hit(factor,status):
+	factor[life_ob[0]] -= factor[atk]*factor[atk_up]
+	print('Beat the monster: "ahah ahhhh..." The monster loses %f points of life.'% (atk*atk_up))
+	factor[atk_up]=1	
 	return factor,1
-def charge(status):
+def charge(factor,status):
 	factor[atk_up]*=1.2 #to be adjusted
-	return factor,2
+	return (factor,2)
 	print("Next round, you'll attack the monster 0.2 stronger than you will originally.")
 def react(factor,status):
-	if status == 1:
-		factor[life_atk_ob[0]] -= factor[atk_ob]*factor[atk_ob_up]
-		print('The monster is attacking you... Be careful...\n"Ahhh"')
+	factor[life_atk_ob[0]] -= factor[atk_ob]*factor[atk_ob_up]
+	print('The monster is attacking you... Be careful...\n"Ahhh"')
 	return factor
