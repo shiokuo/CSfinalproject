9dd3c7d2bd6b4ab24b8df299e89a35f85a9a4508
unknown <a0966530827@gmail.com>
2019-12-02 14:44:48 +0800

    factor

--- logic.py
+++ logic.py
@@ -1,11 +1,10 @@
-import main
-def hit(status):
+def hit(factor,status):
 	if status == 1:
 		factor[life_ob[0]] -= factor[atk]*factor[atk_up]
 		print('Beat the monster: "ahah ahhhh..." The monster loses %f points of life.'% (atk*atk_up))
 		factor[atk_up]=1	
 	return factor,1
-def charge(status):
+def charge(factor,status):
 	factor[atk_up]*=1.2 #to be adjusted
 	return factor,2
 	print("Next round, you'll attack the monster 0.2 stronger than you will originally.")
