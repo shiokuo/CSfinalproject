1a71156391620677c22daf389e7f5019ed768272
Cody Zeng <a0966530827@gmail.com>
2019-12-15 14:36:57 +0800

    try

--- logic.py
+++ logic.py
@@ -1,10 +1,11 @@
+count = 0
 def hit(factor):
 	factor['life_ob'][0] -= factor['atk']*factor['atk_up']
 	print('Beat the monster: "ahah ahhhh..." The monster loses %f points of life.'% (factor['atk'] * factor['atk_up']))
 	factor['atk_up']=1	
 	return factor
 def charge(factor):
-	factor['atk_up']*=1.2 #to be adjusted
+	factor['atk_up']*=1.2 #to be modified
 	print("Next round, you'll attack the monster 0.2 stronger than you will originally.")
 	return factor
 def react(factor,status):
@@ -12,5 +13,6 @@ def react(factor,status):
 		factor['life'] -= factor['atk_ob'][0]*factor['atk_ob_up']
 		print('The monster is attacking you... Be careful...\n"Ahhh"')
 	return factor
+def irritate(factor):
 	
 
