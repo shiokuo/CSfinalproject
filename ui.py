5c86eb9ec569c358ed61d54d44ecbddea49c4bed
shiokuo <b08901175@ntu.edu.tw>
2019-12-15 22:17:37 +0800

    WFFFFF

--- ui.py
+++ ui.py
@@ -23,26 +23,26 @@ def change_plot (status):
         return 1
     
     if status==4:      #OXO
-        my_font = pygame.font.SysFont("arial", 50)
+        my_font = pygame.font.SysFont("arial", 35)
         plot_1=my_font.render('a man who taught Earth Science block your way',999,(255,0,0))
         x=((640-plot_1.get_width()))/2
         y=((480-plot_1.get_height()))/2
         screen.blit(background,(0,0))
         screen.blit(plot_1,(x,y))
         f=my_font.render('press f to continue',999,(255,0,0))
-        screen.blit(f,(x,400))
+        screen.blit(f,(0,400))
         pygame.display.update()
         return 1
     
     if status==8:      #tanya
-        my_font = pygame.font.SysFont("arial", 50)
+        my_font = pygame.font.SysFont("arial", 35)
         plot_1=my_font.render('a chemistry teacher stand in front of you',999,(255,0,0))
         x=((640-plot_1.get_width()))/2
         y=((480-plot_1.get_height()))/2
         screen.blit(background,(0,0))
         screen.blit(plot_1,(x,y))
         f=my_font.render('press f to continue',999,(255,0,0))
-        screen.blit(f,(x,400))
+        screen.blit(f,(0,400))
         pygame.display.update()
         return 1
     
@@ -76,7 +76,7 @@ def fight(status):
         #screen.blit(act,(0,160))
         #screen.blit(tool,(0,320))
         f=my_font.render('1:event 2:skill 3:item',999,(255,0,0))
-        screen.blit(f,(0,420))
+        screen.blit(f,(100,420))
         pygame.display.update()
         return 1
     if status==9:
@@ -88,7 +88,7 @@ def fight(status):
         #screen.blit(act,(0,160))
         #screen.blit(tool,(0,320))
         f=my_font.render('1:event 2:skill 3:item',999,(255,0,0))
-        screen.blit(f,(0,420))
+        screen.blit(f,(100,420))
         pygame.display.update()
         return 1
     else :
@@ -123,46 +123,46 @@ def choice_event(status):
 def choice_battle(status):
     if status[0]!=1 and status[1]!=1:
         my_font = pygame.font.SysFont("arial", 45)
-        plot_1=my_font.render('(q)attack\n(w)charging',999,(255,255,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        plot_1=my_font.render('(q)attack(w)charging',999,(255,255,0))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
     if status[0]==1:
-        my_font = pygame.font.SysFont("arial", 45)
-        plot_1=my_font.render('(q)attack\n(w)charging\n(e)weaken',999,(255,255,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        my_font = pygame.font.SysFont("arial", 35)
+        plot_1=my_font.render('(q)attack(w)charging(e)weaken',999,(255,255,0))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
     if status[1]==1:
-        my_font = pygame.font.SysFont("arial", 45)
-        plot_1=my_font.render('(q)attack\n(w)charging\n(e)weaken\n(r)strong charge',999,(255,255,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        my_font = pygame.font.SysFont("arial", 30)
+        plot_1=my_font.render('(q)attack(w)charging(e)weaken(r)strong charge',999,(255,255,0))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
 def choice_item(status):
     if status[0]!=2 and status[1]!=2:
         my_font = pygame.font.SysFont("arial", 45)
         plot_1=my_font.render('nothing',999,(255,0,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
     if status[0]==2:
         my_font = pygame.font.SysFont("arial", 45)
         plot_1=my_font.render('breakfast(q)',999,(255,0,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
     if status[1]==2:
         my_font = pygame.font.SysFont("arial", 45)
         plot_1=my_font.render('stone statue(w)',999,(255,0,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
-        screen.blit(plot_1,(x,y))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
+        screen.blit(plot_1,(0,370))
         pygame.display.update()
 def feed():
     '''fail'''    
@@ -216,21 +216,21 @@ def hide():
     pygame.display.update()    
 def reward(status):
     if status==2:
-        my_font = pygame.font.SysFont("arial", 45)
-        plot_1=my_font.render('choose your reward\n(z)weaken\n(a)a new breakfast\n(c)a sword',999,(255,0,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
+        my_font = pygame.font.SysFont("arial", 25)
+        plot_1=my_font.render('choose your reward(z)weaken(a)a new breakfast(c)a sword',999,(255,0,0))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
         screen.blit(background,(0,0))
-        screen.blit(plot_1,(x,y))
+        screen.blit(plot_1,(0,420))
         pygame.display.update()
         return 1
     if status==6:
-        my_font = pygame.font.SysFont("arial", 45)
-        plot_1=my_font.render('choose your reward\n(z)strong charge\n(x)stone statue\n(c)shield',999,(255,0,0))
-        x=2*((640-plot_1.get_width()))/3
-        y=((480-plot_1.get_height()))/3
+        my_font = pygame.font.SysFont("arial", 25)
+        plot_1=my_font.render('choose your reward(z)strong charge(x)stone statue(c)shield',999,(255,0,0))
+        #x=2*((640-plot_1.get_width()))/3
+        #y=((480-plot_1.get_height()))/3
         screen.blit(background,(0,0))
-        screen.blit(plot_1,(x,y))
+        screen.blit(plot_1,(0,420))
         pygame.display.update()
         return 1
 
